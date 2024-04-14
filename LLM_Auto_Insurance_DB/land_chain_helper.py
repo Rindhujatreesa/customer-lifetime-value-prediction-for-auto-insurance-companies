98b1c61cf6abc5d27cc299f129f3b13869f813f4
Nithin <52503252+nithin42@users.noreply.github.com>
2024-04-13 20:58:55 -0400

    Add files via upload

---
+++ LLM_Auto_Insurance_DB/land_chain_helper.py
@@ -0,0 +1,28 @@
+from langchain.llms import GooglePalm
+from langchain.utilities import SQLDatabase
+from langchain_experimental.sql import SQLDatabaseChain
+from langchain.prompts import SemanticSimilarityExampleSelector
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.prompts import FewShotPromptTemplate
+from langchain.chains.sql_database.prompt import PROMPT_SUFFIX, _mysql_prompt
+from langchain.prompts.prompt import PromptTemplate
+
+
+import os
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env (especially openai api key)
+
+
+def db_chain():
+    db_user = "root"
+    db_password = "1234"
+    db_host = "localhost"
+    db_name = "Auto_insurance"
+
+    db = SQLDatabase.from_uri(f"mysql+pymysql://{db_user}:{db_password}@{db_host}/{db_name}",sample_rows_in_table_info=3)
+    llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
+
+    db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
+
+    return db_chain
---
+++ LLM_Auto_Insurance_DB/main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from land_chain_helper import db_chain
+
+st.title("Auto insurance: Database Q&A ")
+
+question = st.text_input("Question: ")
+
+if question:
+    chain = db_chain()
+    response = chain.run(question)
+
+    st.header("Answer")
+    st.write(response)
